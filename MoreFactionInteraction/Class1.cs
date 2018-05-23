09499bca78798c00c2e631a8ea8f074229009e4e
Mehni <Mehni@users.noreply.github.com>
2018-05-23 14:32:02 +0200

    Add project files.

---
+++ MoreFactionInteraction/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Harmony;
+
+namespace MoreFactionInteraction
+{
+    public class Class1
+    {
+    }
+}
