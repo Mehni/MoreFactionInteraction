6384467fe088d4ab26eee651ec1f0c09c4de9da5
Mehni <Mehni@users.noreply.github.com>
2018-08-03 15:23:49 +0200

    bumper crop reward balance tweak

--- MoreFactionInteraction/World Incidents/WorldObjectComp_SettlementBumperCropComp.cs
+++ MoreFactionInteraction/World Incidents/WorldObjectComp_SettlementBumperCropComp.cs
@@ -12,7 +12,7 @@ namespace MoreFactionInteraction.World_Incidents
     {
         //attentive readers may find similarities between this and the Peacetalks quest. 
         public int expiration = -1;
-        private const int basereward = 20;
+        private const int basereward = 50;
         public int workLeft;
         private bool workStarted;
         private const int workAmount = GenDate.TicksPerDay;
@@ -69,7 +69,9 @@ namespace MoreFactionInteraction.World_Incidents
             float totalYieldPowerForCaravan = CalculateYieldForCaravan(caravanMembersCapableOfGrowing: allMembersCapableOfGrowing);
 
             //TODO: Calculate a good amount
-            float totalreward = basereward * totalYieldPowerForCaravan * (allMembersCapableOfGrowing.Count 
+            //v1 (first vers): base of 20 * Sum of plant harvest yield * count * avg grow skill (20 * 2.96 * 3 * 14.5) = ~2579 or 20*5.96*6*20 = 14400
+            //v2 (2018/08/03): base of 50 * avg of plant harvest yield * count * avg grow skill (50 * 0.99 * 3 * 14.5) = ~2153 or 40*0.99*6*20 = 4752
+            float totalreward = basereward * totalYieldPowerForCaravan * (allMembersCapableOfGrowing.Count
                               * Mathf.Max(a: 1, b: (float)allMembersCapableOfGrowing.Average(selector: pawn => pawn.skills.GetSkill(skillDef: SkillDefOf.Plants).Level)));
 
             Thing reward = ThingMaker.MakeThing(def: RandomRawFood());
@@ -85,16 +87,16 @@ namespace MoreFactionInteraction.World_Incidents
             allMembersCapableOfGrowing.ForEach(action: pawn => pawn.skills.Learn(sDef: SkillDefOf.Plants, xp: expGain, direct: true));
         }
 
-		//a long list of things to excluse stuff like milk and kibble. In retrospect, it may have been easier to get all plants and get their harvestables.
-		private static ThingDef RandomRawFood() => (from x in ThingSetMakerUtility.allGeneratableItems
-													where x.IsNutritionGivingIngestible && !x.IsCorpse && x.ingestible.HumanEdible && !x.IsMeat
-														&& !x.IsDrug && !x.HasComp(compType: typeof(CompHatcher)) && !x.HasComp(compType: typeof(CompIngredients))
-														&& x.BaseMarketValue < 3 && (x.ingestible.preferability == FoodPreferability.RawBad || x.ingestible.preferability == FoodPreferability.RawTasty)
-													select x).RandomElementWithFallback(ThingDefOf.RawPotatoes);
+        //a long list of things to excluse stuff like milk and kibble. In retrospect, it may have been easier to get all plants and get their harvestables.
+        private static ThingDef RandomRawFood() => (from x in ThingSetMakerUtility.allGeneratableItems
+                                                    where x.IsNutritionGivingIngestible && !x.IsCorpse && x.ingestible.HumanEdible && !x.IsMeat
+                                                        && !x.IsDrug && !x.HasComp(compType: typeof(CompHatcher)) && !x.HasComp(compType: typeof(CompIngredients))
+                                                        && x.BaseMarketValue < 3 && (x.ingestible.preferability == FoodPreferability.RawBad || x.ingestible.preferability == FoodPreferability.RawTasty)
+                                                    select x).RandomElementWithFallback(ThingDefOf.RawPotatoes);
 
-		private static float CalculateYieldForCaravan(IEnumerable<Pawn> caravanMembersCapableOfGrowing)
+        private static float CalculateYieldForCaravan(IEnumerable<Pawn> caravanMembersCapableOfGrowing)
         {
-            return caravanMembersCapableOfGrowing.Select(selector: x => x.GetStatValue(stat: StatDefOf.PlantHarvestYield)).Sum();
+            return caravanMembersCapableOfGrowing.Select(selector: x => x.GetStatValue(stat: StatDefOf.PlantHarvestYield)).Average();
         }
 
         private static string GetLetterText(string baseText, Caravan caravan)
@@ -114,7 +116,7 @@ namespace MoreFactionInteraction.World_Incidents
 
         private static List<Pawn> AllCaravanMembersCapableOfGrowing(Caravan caravan)
         {
-            return caravan.PawnsListForReading.Where(predicate: pawn => !pawn.Dead && !pawn.Downed && !pawn.InMentalState 
+            return caravan.PawnsListForReading.Where(predicate: pawn => !pawn.Dead && !pawn.Downed && !pawn.InMentalState
                                                                      && caravan.IsOwner(p: pawn) && pawn.health.capacities.CanBeAwake
                                                                      && !StatDefOf.PlantHarvestYield.Worker.IsDisabledFor(thing: pawn)).ToList();
         }
--- README.md
+++ README.md
@@ -53,6 +53,7 @@ Shoutout to [Albion of Sparkling Worlds](https://steamcommunity.com/sharedfiles/
 ## Language
 
 - Japanese, courtesy of Proxyer
+- Russian, courtsey of Djeban74/Reasonable Max
 
 ## Links
 
