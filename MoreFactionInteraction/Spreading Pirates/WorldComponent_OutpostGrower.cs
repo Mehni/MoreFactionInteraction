26079cdb494c4154110246ae669387ea5857ceda
Mehni <Mehni@users.noreply.github.com>
2018-09-09 17:04:27 +0200

    fix upgrade of outposts with active map

--- MoreFactionInteraction/Spreading Pirates/WorldComponent_OutpostGrower.cs
+++ MoreFactionInteraction/Spreading Pirates/WorldComponent_OutpostGrower.cs
@@ -17,10 +17,12 @@ namespace MoreFactionInteraction
             base.WorldComponentUpdate();
             if (Find.TickManager.TicksGame % 100 == 0)
             {
+                bool alsoRemoveWorldObject;
                 //get settlements to upgrade. These shouldn't include temp generated or event maps -- preferably only the outposts this spawned by this mod
                 //ideally I'd add some specific Component to each outpost (as a unique identifier and maybe even as the thing that makes em upgrade), but for the moment that's not needed.
                 IEnumerable<Site> sites = from site in Find.WorldObjects.Sites
                                           where site.Faction.HostileTo(other: Faction.OfPlayer) && site.Faction.def.permanentEnemy && !site.Faction.def.hidden && !site.Faction.defeated
+                                          && site.ShouldRemoveMapNow(out alsoRemoveWorldObject)
                                           && site.parts.Any(predicate: (SitePart x) => x.Def == SitePartDefOf.Outpost) && !site.GetComponent<TimeoutComp>().Active
                                           select site;
 
