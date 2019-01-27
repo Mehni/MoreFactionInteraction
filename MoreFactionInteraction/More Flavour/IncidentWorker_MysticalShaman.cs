752c919161014b8e292543b9f47ade379e7f46d7
Mehni <Mehni@users.noreply.github.com>
2019-01-27 23:55:23 +0100

    first choiceletter gone

--- MoreFactionInteraction/More Flavour/IncidentWorker_MysticalShaman.cs
+++ MoreFactionInteraction/More Flavour/IncidentWorker_MysticalShaman.cs
@@ -8,6 +8,8 @@ using RimWorld.Planet;
 
 namespace MoreFactionInteraction
 {
+    using More_Flavour;
+
     class IncidentWorker_MysticalShaman : IncidentWorker
     {
         private static List<Map> tmpAvailableMaps = new List<Map>();
@@ -17,15 +19,16 @@ namespace MoreFactionInteraction
         private const int MinDistance = 8;
         private const int MaxDistance = 22;
         private const int TimeoutTicks = GenDate.TicksPerDay;
+        private static readonly IntRange TimeoutDaysRange = new IntRange(min: 5, max: 15);
 
         protected override bool CanFireNowSub(IncidentParms parms)
         {
-            return base.CanFireNowSub(parms: parms) && Find.AnyPlayerHomeMap != null 
+            return base.CanFireNowSub(parms: parms) && Find.AnyPlayerHomeMap != null
                                              && !Find.WorldObjects.AllWorldObjects.Any(predicate: o => o.def == MFI_DefOf.MFI_MysticalShaman)
-                                             && Find.FactionManager.AllFactionsVisible.Where(predicate: f => f.def.techLevel <= TechLevel.Neolithic 
+                                             && Find.FactionManager.AllFactionsVisible.Where(predicate: f => f.def.techLevel <= TechLevel.Neolithic
                                                                                                && !f.HostileTo(other: Faction.OfPlayer)).TryRandomElement(result: out Faction result)
                                              && TryFindTile(tile: out int num)
-                                             && TryGetRandomAvailableTargetMap (map: out Map map)
+                                             && TryGetRandomAvailableTargetMap(map: out Map map)
                                              && CommsConsoleUtility.PlayerHasPoweredCommsConsole();
         }
 
@@ -49,15 +52,49 @@ namespace MoreFactionInteraction
 
             int fee = Rand.RangeInclusive(min: 400, max: 1000);
 
-            ChoiceLetter_MysticalShaman choiceLetterMysticalShaman = (ChoiceLetter_MysticalShaman)LetterMaker.MakeLetter(label: this.def.letterLabel, text: "MFI_MysticalShamanLetter".Translate(faction.Name, fee.ToString()), def: this.def.letterDef);
-            choiceLetterMysticalShaman.title = "MFI_MysticalShamanTitle".Translate().CapitalizeFirst();
-            choiceLetterMysticalShaman.faction = faction;
-            choiceLetterMysticalShaman.tile = tile;
-            choiceLetterMysticalShaman.map = map;
-            choiceLetterMysticalShaman.fee = fee;
-            choiceLetterMysticalShaman.StartTimeout(duration: TimeoutTicks);
-            Find.LetterStack.ReceiveLetter(let: choiceLetterMysticalShaman);
-            Find.World.GetComponent<WorldComponent_OutpostGrower>().Registerletter(choiceLetterMysticalShaman);
+            //ChoiceLetter_MysticalShaman choiceLetterMysticalShaman = (ChoiceLetter_MysticalShaman)LetterMaker.MakeLetter(label: this.def.letterLabel, text: "MFI_MysticalShamanLetter".Translate(faction.Name, fee.ToString()), def: this.def.letterDef);
+            //choiceLetterMysticalShaman.title = "MFI_MysticalShamanTitle".Translate().CapitalizeFirst();
+            //choiceLetterMysticalShaman.faction = faction;
+            //choiceLetterMysticalShaman.tile = tile;
+            //choiceLetterMysticalShaman.map = map;
+            //choiceLetterMysticalShaman.fee = fee;
+            //choiceLetterMysticalShaman.StartTimeout(duration: TimeoutTicks);
+            //Find.LetterStack.ReceiveLetter(let: choiceLetterMysticalShaman);
+            //Find.World.GetComponent<WorldComponent_OutpostGrower>().Registerletter(choiceLetterMysticalShaman);
+
+            DiaNode diaNode = new DiaNode("MFI_MysticalShamanLetter".Translate(faction.Name, fee.ToString()));
+            DiaOption accept = new DiaOption(text: "RansomDemand_Accept".Translate())
+            {
+                action = () =>
+                         {
+                             MysticalShaman mysticalShaman = (MysticalShaman)WorldObjectMaker.MakeWorldObject(def: MFI_DefOf.MFI_MysticalShaman);
+                             mysticalShaman.Tile = tile;
+                             mysticalShaman.SetFaction(newFaction: faction);
+                             int randomInRange = TimeoutDaysRange.RandomInRange;
+                             mysticalShaman.GetComponent<TimeoutComp>().StartTimeout(ticks: randomInRange * GenDate.TicksPerDay);
+                             Find.WorldObjects.Add(o: mysticalShaman);
+                             TradeUtility.LaunchSilver(map: map, fee: fee);
+                         },
+                resolveTree = true
+            };
+            if (!TradeUtility.ColonyHasEnoughSilver(map: map, fee: fee))
+            {
+                accept.Disable(newDisabledReason: "NeedSilverLaunchable".Translate(fee.ToString()));
+            }
+
+            DiaOption reject = new DiaOption(text: "RansomDemand_Reject".Translate())
+            {
+                action = () =>
+                         {
+
+                         },
+                resolveTree = true
+            };
+            diaNode.options = new List<DiaOption> {accept, reject};
+
+            Find.WindowStack.Add(new Dialog_NodeTree(diaNode, title: this.def.letterLabel));
+            Find.Archive.Add(new ArchivedDialog(diaNode.text, this.def.letterLabel));
+
             return true;
         }
 
